a50f5716e59b0ae44909774c68c60eb669b7aa9c
Jesús de Sosa Cruz <0619001278@alu.uma.es>
2013-03-13 16:16:24 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -12,16 +12,16 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-	System.out.println("modificado por alum 1"); // insertado por alum1
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("modificado por alum 2"); // insertado por alum2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificacaciones alumno 1
-	System.out.println("Alumno 1 inserta esta sentencia");
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
     }
 }
